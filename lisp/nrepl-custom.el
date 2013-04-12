c733559b0a9dde0bbc584f555da3791b04c834bf
Andy Kitchen <kitchen.andy@gmail.com>
2013-04-12 22:25:48 +1000

    more sophisticated nrepl-run-again

--- lisp/nrepl-custom.el
+++ lisp/nrepl-custom.el
@@ -1,23 +1,7 @@
-;; (provide 'nrepl-custom)
-
 ;; (add-hook 'nrepl-interaction-mode-hook
 ;;   'nrepl-turn-on-eldoc-mode)
 
 (add-to-list 'same-window-buffer-names "*nrepl*")
-
-(defun my-nrepl-err-handler (buffer ex root-ex session)
-  (with-current-buffer buffer
-    (nrepl-send-string 
-     "(if-let [pst+ (clojure.core/resolve 'clj-stacktrace.repl/pst+)]
-                      (pst+ *e) (clojure.stacktrace/print-stack-trace *e))"
-     (nrepl-make-response-handler
-      (nrepl-make-popup-buffer nrepl-error-buffer)
-      nil
-      'nrepl-emit-into-color-buffer nil nil) nil session)))
-
-(eval-after-load "nrepl"
-  '(progn
-     (setq-default nrepl-err-handler 'my-nrepl-err-handler)))
 
 (defun nrepl-save-and-load-current-buffer ()
   (interactive)
@@ -31,6 +15,7 @@
 
 (make-variable-buffer-local 'nrepl-test-namespaces)
 
+;; TODO Fixme
 (defun nrepl-run-tests ()
   (interactive)
   (save-buffer)
@@ -41,10 +26,38 @@
       (insert "(clojure.test/run-tests " namespaces ")")
       (nrepl-return t))))
 
+(defun run-again-nrepl-load-file-handler (buffer)
+  (let (current-ns (nrepl-current-ns))
+    (nrepl-make-response-handler
+     buffer
+     (lambda (buffer value)
+       (message "%s" value)
+       (with-current-buffer buffer
+         (setq nrepl-buffer-ns (clojure-find-ns))))
+     (lambda (buffer value)
+       (nrepl-emit-interactive-output value))
+     (lambda (buffer err)
+       (message "%s" err))
+     (lambda (buffer)
+       (with-current-buffer (get-buffer nrepl-nrepl-buffer)
+         (nrepl-replace-input (car nrepl-input-history))
+         (nrepl-send-input t))))))
+
+(defun run-again-nrepl-load-file-op (filename)
+  (run-again-nrepl-send-load-file (nrepl-file-string filename)
+                               filename
+                               (file-name-nondirectory filename)))
+
+(defun run-again-nrepl-send-load-file (file-contents file-path file-name)
+  (let ((buffer (current-buffer)))
+    (nrepl-send-request (list "op"        "load-file"
+                              "session"   (nrepl-current-session)
+                              "file"      file-contents
+                              "file-path" file-path
+                              "file-name" file-name)
+                        (run-again-nrepl-load-file-handler buffer))))
+
 (defun nrepl-run-again ()
   (interactive)
   (save-buffer)
-  (nrepl-load-current-buffer)
-  (with-current-buffer (get-buffer "*nrepl*")
-    (nrepl-replace-input (car nrepl-input-history))
-    (nrepl-return t)))
+  (run-again-nrepl-load-file-op (buffer-file-name)))
